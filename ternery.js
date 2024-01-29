6a059754241ec8ebbf5b36edc436f31379c0f720
Tasin Evan <tasinmosleh508@gmail.com>
2024-01-30 03:23:21 +0600

    ternery

---
+++ ternery.js
@@ -0,0 +1,25 @@
+
+var num1 =10;
+var num2 = 5 ;
+var result ;
+
+
+
+// if (num1 > num2) {
+//     result = num1 * 2;
+// } else {
+//     result = num1 + num2;
+// }
+
+// console.log(result);
+
+
+
+
+result= num1 > num2 ? result = num1 * 2 :  num1 + num2;
+console.log(result);
+
+
+
+
+
